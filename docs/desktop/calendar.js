6127be3b3b60274005c29aab23697108c7531c7f
Ryan Yang <javathon@gmail.com>
2018-08-26 11:59:19 -0700

    deleted server code

--- docs/desktop/calendar.js
+++ docs/desktop/calendar.js
@@ -1,16 +1,24 @@
 const date = new Date();
 const daysPerMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+const dayNames = ['Sun', 'Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat'];
 // const 
 
 function createCalendar(){
-    for(let j = 0; j < 5; j++){
+
+    for(let j = -1; j < 5; j++){
         for(let i = 0; i < 7; i++){
-            let day = document.getElementById('dates--interactive').appendChild(document.createElement("div"));
-            day.id = 'day' + (j*7 + i);
-            day.className = 'calendar__day'
+            if(j < 0){
+                let day = document.getElementById('dates--interactive').appendChild(document.createElement("div"));
+                day.className = 'calendar__day';
+                day.textContent = dayNames[i];
+            }else{
+                let day = document.getElementById('dates--interactive').appendChild(document.createElement("div"));
+                day.id = 'day' + (j*7 + i);
+                day.className = 'calendar__day';
+            }
         }
         let clear = document.getElementById('dates--interactive').appendChild(document.createElement("div"));
-        clear.className = 'calendar__day--clear'
+        clear.className = 'calendar__day--clear';
     }
     createMonth(date.getMonth(), date.getFullYear());
 }
