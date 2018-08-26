ae9d76bc900a1e9ca23bdbf21e84f77d70dbabc9
Ryan Yang <javathon@gmail.com>
2018-08-25 17:46:03 -0700

    added qr code generator

--- docs/desktop/Host/script.js
+++ docs/desktop/Host/script.js
@@ -1,6 +1,29 @@
-const ID_SIZE = 8;                                                  //length of generated room code
-const LINK_URL = 'https://httprelay.io/link/' + generateRoomID();   //generated url
+const ID_SIZE = 20;
+const httpRelayBaseUrl = 'https://httprelay.io/mcast/';
 
+/** @todo: use these for production */
+// const ROOM_ID = generateRoomID();
+// const LINK_URL = httpRelayBaseUrl + ROOM_ID;
+
+const ROOM_ID = '1t78pQ0KDxGeuI1OKHbR';
+const LINK_URL = 'https://httprelay.io/mcast/1t78pQ0KDxGeuI1OKHbR';
+
+const QR_COLOR = '506070';
+const QR_BGCOLOR = 'D8FBD8';
+// const QR_COLOR = '000000';
+// const QR_BGCOLOR = 'FFFFFF';
+const QR_SIZE = '300x300';
+const QR_FORMAT = 'svg';
+
+const  qrCodeBaseUrl
+    = 'https://api.qrserver.com/v1/create-qr-code/'
+    + '?size='      + QR_SIZE
+    + '&format='    + QR_FORMAT
+    + '&color='     + QR_COLOR
+    + '&bgcolor='   + QR_BGCOLOR
+    + '&data=';
+
+ 
 function generateRoomID(){
     let roomID = "";
     const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -12,22 +35,26 @@ function generateRoomID(){
 
 function load(){
     console.log(LINK_URL);
-    const message = "Hello World";
-    sendSchedule(LINK_URL, message);
-    apiCall(LINK_URL, message);
+    // sendSchedule(LINK_URL, message);
+    // sendSchedule(LINK_URL, "new message");
+    // apiCall(LINK_URL);
+    generateQRCode();
 }
 
+function generateQRCode(){
+    document.getElementById('qr').src = qrCodeBaseUrl + ROOM_ID;
+}
 
-function apiCall(link, message){
+function getRequest(link, asyncFunc){
     axios.get(link)
     .then(function (response) {
         console.log(response);
+        // asyncFunc(response);
     })
     .catch(function (error) {
         console.log(error);
     })
     .then(function(){
-
     });
 }
 
