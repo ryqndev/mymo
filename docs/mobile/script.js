b21cf57a2ed41ac44d783f36cbfcaeea8d54fbbf
Ryan Yang <javathon@gmail.com>
2018-09-06 13:56:36 -0700

    added basic ripple animation to buttons

--- docs/mobile/script.js
+++ docs/mobile/script.js
@@ -1,7 +1,33 @@
+function load(){
+    mdc.ripple.MDCRipple.attachTo(document.querySelector('.mdc-button--outlined'));
+}
 function link(type){
-    if(type === 'host'){
-        
+    let shsD =  document.getElementById('section--holder-selection').style.display;
+    let shhD =  document.getElementById('section--holder-host').style.display;
+    let shcD =  document.getElementById('section--holder-client').style.display;
+    if(type === 0){
+        shsD = 'visible';
+        shhD = 'none';
+        shcD = 'none';
+    }else if(type === 1){
+        shsD = 'none';
+        shhD = 'visible';
+        shcD = 'none';
+    }else if(type === 2){
+        shsD = 'none';
+        shhD = 'none';
+        shcD = 'visible';
+    }else if(type === 3){
+        window.location.href = './Host/index.html';
+    }
+    else if(type === 4){
+        window.location.href = './Client/index.html';
     }
 }
-mdc.ripple.MDCRipple.attachTo(document.querySelector('.mdc-button--outlined'));
+
+// const divs = document.getElementsByTagName("div");
+// for(var i = 0; i < divs.length; i++){
+//    //do something to each div like
+//    divs[i].innerHTML = "something new...";
+// }
 // const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
