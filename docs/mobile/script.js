3edeffa533299ae524aad431353848999d2df98a
Ryan Yang <javathon@gmail.com>
2018-09-06 12:15:44 -0700

    created dynamic page routing without reload template

--- docs/mobile/script.js
+++ docs/mobile/script.js
@@ -1,4 +1,7 @@
-function isHost(){
-    // windo
+function link(type){
+    if(type === 'host'){
+        
+    }
 }
 mdc.ripple.MDCRipple.attachTo(document.querySelector('.mdc-button--outlined'));
+// const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
