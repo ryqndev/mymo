00d464699598d9d0a7e02637857150b14dc6ab9f
Ryan Yang <javathon@gmail.com>
2018-10-08 18:48:56 -0700

    realized i forgot reponsive meta tag which is the reason why it kept showing a 981 device width

--- docs/script.js
+++ docs/script.js
@@ -1,9 +1,9 @@
 function redirect(){
     var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
     alert(width);
-    // if(width <= 800){
-    //     window.location = 'mobile/index.html';
-    // }else{
-    //     window.location = 'desktop/index.html';
-    // }
+    if(width <= 1000){
+        window.location = 'mobile/index.html';
+    }else{
+        window.location = 'desktop/index.html';
+    }
 }
