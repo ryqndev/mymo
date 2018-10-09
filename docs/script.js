052c087f7050554c16cc0d7211a22d94146062b0
Ryan Yang <javathon@gmail.com>
2018-10-08 18:50:40 -0700

    meta tag was the problem. on a pixel 2 xl it shows 412 px which is the correct physical device widthg

--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,6 @@
 function redirect(){
     var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    alert(width);
-    if(width <= 1000){
+    if(width <= 700){
         window.location = 'mobile/index.html';
     }else{
         window.location = 'desktop/index.html';
