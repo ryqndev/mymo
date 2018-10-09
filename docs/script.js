914316b1973681b128e8747c93f2832499dfae15
Ryan Yang <javathon@gmail.com>
2018-10-08 18:45:19 -0700

    finding out width of pixel 2 xl

--- docs/script.js
+++ docs/script.js
@@ -1,9 +1,9 @@
 function redirect(){
     var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    console.log(width);
-    if(width <= 800){
-        window.location = 'mobile/index.html';
-    }else{
-        window.location = 'desktop/index.html';
-    }
+    // alert(width);
+    // if(width <= 800){
+    //     window.location = 'mobile/index.html';
+    // }else{
+    //     window.location = 'desktop/index.html';
+    // }
 }
