d66bb5311e1a4e9ea641c2ea464f64758bafd5c9
Ryan Yang <javathon@gmail.com>
2018-10-08 18:46:10 -0700

    added wrong thing

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,6 @@
 function redirect(){
     var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    // alert(width);
+    alert(width);
     // if(width <= 800){
     //     window.location = 'mobile/index.html';
     // }else{
