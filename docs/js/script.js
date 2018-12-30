a387d3bfbc2cafa935a018c744671168bd13a1dc
Ryan Yang <javathon@gmail.com>
2018-12-29 18:24:16 -0800

    timepicker implementation for time. also opting to do it by the minute since calculations for a 2 week time period dureing regular waking hours will take up at most 0.1mb of memory

--- docs/js/script.js
+++ docs/js/script.js
@@ -4,55 +4,93 @@
 const JOIN_CODE = 20;
 
 function load(){
+    setupDatepickers();
+    setupTimepickers();
+}
+function setupDatepickers(){
     let options = {
         'onClose': checkEndDate,
         'minDate': new Date(),
         'format': 'mm/dd/yyyy',
         'container': document.body
     }
-    var elems = document.querySelectorAll('.datepicker');
-    var instances = M.Datepicker.init(elems, options);
+    let elems = document.querySelectorAll('.datepicker');
+    let instances = M.Datepicker.init(elems, options);
 }
-
+function setupTimepickers(){
+    let options = {
+        'onCloseEnd': checkEndTime
+    }
+    let elems = document.querySelectorAll('.timepicker');
+    let instances = M.Timepicker.init(elems, options);
+}
 function link(type){
     let shsD =  document.getElementById('section--holder-selection');
     let shhD =  document.getElementById('section--holder-host');
+    let shh2 =  document.getElementById('section--holder-host2');
     let shcD =  document.getElementById('section--holder-client');
-    if(type === 0){
-        shsD.style.display = 'block';
-        shhD.style.display = 'none';
-        shcD.style.display = 'none';
-    }else if(type === 1){
-        shsD.style.display = 'none';
-        shhD.style.display = 'block';
-        shcD.style.display = 'none';
-    }else if(type === 2){
-        shsD.style.display = 'none';
-        shhD.style.display = 'none';
-        shcD.style.display = 'block';
-    }else if(type === 3){
-        let roomURL = document.getElementById('text--client').value;
-        //check if url is legit
-        let urlLen = roomURL.length;
-        let roomCode = roomURL.substr(urlLen - JOIN_CODE, JOIN_CODE);
-        let curURL = window.location.href;
-        window.location.href = './r/?id=' + roomCode;
-        // window.location.href = roomURL;
-    }
-    else if(type === 4){
-        if(document.getElementById('end-date').classList.contains('valid')){
-            let startDate = document.getElementById('start-date').value;
-            let endDate =  document.getElementById('end-date').value;
-            window.location.href = `./r/?sd=${startDate}&ed=${endDate}`;
-        }else{
-            document.getElementById('end-date').classList.add('shake');
-
-            setTimeout(function(){document.getElementById('end-date').classList.remove('shake')}, 1000);
-        }
-        
+    switch(type){
+        case 0:
+            shcD.classList.add('fade-out');
+            shhD.classList.add('fade-out');
+            setTimeout(function(){
+                shhD.style.display = 'none';
+                shcD.style.display = 'none';
+                shsD.style.display = 'block'
+                shcD.classList.remove('fade-out');
+                shhD.classList.remove('fade-out');
+                shsD.classList.add('fade-in');
+            }, 320);
+            break;
+        case 1:
+            shsD.classList.add('fade-out');
+            setTimeout(function(){
+                shsD.style.display = 'none';
+                shsD.classList.remove('fade-out');
+                shcD.style.display = 'block';
+                shcD.classList.add('fade-in');
+            }, 320);
+            break;
+        case 2:
+            shsD.classList.add('fade-out');
+            shh2.classList.add('fade-out');
+            setTimeout(function(){
+                shsD.style.display = 'none';
+                shh2.style.display = 'none';
+                shsD.classList.remove('fade-out');
+                shh2.classList.remove('fade-out');
+                shhD.style.display = 'block';
+                shhD.classList.add('fade-in');
+            }, 320);
+            break;
+        case 3:
+            let roomURL = document.getElementById('text--client').value;
+            let urlLen = roomURL.length;
+            let roomCode = roomURL.substr(urlLen - JOIN_CODE, JOIN_CODE);
+            window.location.href = `./r/?id=${roomCode}`;
+            break;
+        case 4:
+            if(document.getElementById('end-date').classList.contains('valid')){
+                let startDate = document.getElementById('start-date').value;
+                let endDate =  document.getElementById('end-date').value;
+                // let url = `https://planmy.social/r/?sd=${startDate}&ed=${endDate}`;
+                shhD.classList.remove('fade-in');
+                shhD.classList.add('fade-out');
+                setTimeout(function(){
+                    shhD.style.display = 'none';
+                    shhD.classList.remove('fade-out');
+                    shh2.style.display = 'block';
+                    shh2.classList.add('fade-in');
+                }, 320);
+            }else{
+                document.getElementById('end-date').classList.add('shake');
+                setTimeout(function(){document.getElementById('end-date').classList.remove('shake')}, 1000);
+            }
+            break;
+        case 5:
+            break;
     }
 }
-
 function checkEndDate(){
     let startDate = document.getElementById('start-date').value;
     let endDate = document.getElementById('end-date').value;
@@ -61,5 +99,27 @@ function checkEndDate(){
     }else{
         document.getElementById('end-date').className = 'datepicker active invalid';  
     }
+}
 
+function checkEndTime(){
+    //11:00 PM
+    let startTime = document.getElementById('start-time').value;
+    let endTime = document.getElementById('end-time').value;
+    let startMinutes = timeToMinutes(startTime);
+    let endMinutes = timeToMinutes(endTime);
+    if((startTime != "" ) && (endTime != "") && (startMinutes < endMinutes)){
+        document.getElementById('end-time').className = 'timepicker active valid';  
+    }else{
+        document.getElementById('end-time').className = 'timepicker active invalid';  
+    }
+}
+/**
+ * returns the number of minutes elapsed since 12 AM or 00:00:00
+ */
+function timeToMinutes(time){
+    let minutes = time.substr(3, 2);
+    let hours = parseInt(time.substr( 0, 2 ))%12 + parseInt( time.charAt(6) == 'A' ? 0 : 12 );
+    console.log(hours, minutes);
+
+    return parseInt(minutes) + (60*hours);
 }
