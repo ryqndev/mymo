3df2593b19719ac1bae5c26f6d8da1be965b5a37
Ryan Yang <javathon@gmail.com>
2019-01-05 13:20:27 -0800

    small ui test

--- docs/r/js/script.js
+++ docs/r/js/script.js
@@ -1,4 +1,8 @@
 function load(){
+    setTimeout(function(){
+        // This hides the address bar:
+        window.scrollTo(0, 1);
+    }, 0);
     let params = (new URL(document.location)).searchParams;
     sd = new Date(params.get("sd").substr(6,4), params.get("sd").substr(0,2) - 1, params.get("sd").substr(3,2));
     ed = new Date(params.get("ed").substr(6,4), params.get("ed").substr(0,2) - 1, params.get("ed").substr(3,2));
