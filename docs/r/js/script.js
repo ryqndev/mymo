2339cc7ee18f7c46778fb8133c5054989480c897
Ryan Yang <javathon@gmail.com>
2019-01-06 10:08:53 -0800

    testing pwa live functionality

--- docs/r/js/script.js
+++ docs/r/js/script.js
@@ -1,3 +1,4 @@
+let infoToggle = false;
 function load(){
     let params = (new URL(document.location)).searchParams;
     sd = new Date(params.get("sd").substr(6,4), params.get("sd").substr(0,2) - 1, params.get("sd").substr(3,2));
@@ -7,13 +8,9 @@ function load(){
     let options = {};
     let elems = document.querySelectorAll('.tooltipped');
     let instances = M.Tooltip.init(elems, options);
-    document.getElementById('info').addEventListener('mousedown', function(el) {
+    document.getElementById('info').addEventListener('onclick', function(el) {
         el.preventDefault();
-        info(true);
-    });
-    document.getElementById('info').addEventListener('mouseup', function(el) {
-        el.preventDefault();
-        info(false);
+        info();
     });
 }
 function storage(){
@@ -28,8 +25,9 @@ function edit(){
 function finish(){
 
 }
-function info(press){
+function info(){
     document.querySelectorAll('.tooltipped').forEach(e => {
-        press?M.Tooltip.getInstance(e).open():M.Tooltip.getInstance(e).close();
+        infoToggle?M.Tooltip.getInstance(e).open():M.Tooltip.getInstance(e).close();
     })
+    infoToggle = !infoToggle;
 }
