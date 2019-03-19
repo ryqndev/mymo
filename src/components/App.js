2eba7b0692450a445e4fc18fdd195fafff544e90
Ryan Yang <javathon@gmail.com>
2019-03-19 15:44:10 -0700

    i broke something

--- src/components/App.js
+++ src/components/App.js
@@ -17,7 +17,7 @@ class App extends Component {
         return (
             <Provider store={store}>
                 <Background />
-                <Router basename={process.env.PUBLIC_URL}>
+                <Router>
                     <div>
                         <Route exact strict path='/' component={EntryAppInterface}/>
                         <Route path='/:room' component={AppInterface} />
