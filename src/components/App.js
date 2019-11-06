59ddfc79acb2e663fb1ac58c5468d9a1890d9c12
Ryan Yang <javathon@gmail.com>
2019-11-06 12:42:06 -0800

    refactoring entire code base with react hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,9 @@
-import React, { Component } from 'react';
-import {HashRouter as Router, Route} from 'react-router-dom';
-import {AppInterface as EntryAppInterface} from '../entry/components/app-interface';
-import {AppInterface as PlanAppInterface} from '../plan/components/app-interface';
-import './styles/background.css';
+import React from 'react';
 
-class App extends Component {
-    render() {
-        return (
-            <div className="background">
-                <Router basename={process.env.PUBLIC_URL}>
-                    <div>
-                        <Route exact strict path='/' component={EntryAppInterface}/>
-                        <Route path='/:room' component={PlanAppInterface} />
-                    </div>
-                </Router>
-            </div>
-        );
-    }
+const App = () => {
+    return (
+        <div>Hello</div>
+    );
 }
 
 export default App;
